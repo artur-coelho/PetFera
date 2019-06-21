698df86f7574f24449df2ca8acf84c36b43bde7d
Song Jong Márcio Simioni da Costa <janeirode97@gmail.com>
2019-06-21 17:00:40 -0300

    Troca do cin pelo getline

--- src/Interface.cpp
+++ src/Interface.cpp
@@ -195,32 +195,37 @@ void Interface::cadastrarFuncionario() {
 	switch(opt) {
 		case 'v':
 			std::cout << 
-			"Qual o CRMV do veterinario?" << std::endl << "> ";
-			std::cin >> CRMV;
+			"Qual o CRMV do veterinario? (string)" << std::endl << "> ";
+			std::cin.ignore();
+			std::getline(cin, CRMV);
 			break;
 		case 't':
 			std::cout <<
-			"Qual o nivel de seguranca do tratador?" << std::endl << "> ";
+			"Qual o nivel de seguranca do tratador? (int)" << std::endl << "> ";
 			std::cin >> nivelSeguranca;
 			break;
 		default:
 			optInvalida();
 			cadastrarFuncionario();
 	}
-	std::cout << "Qual o ID do novo funcionario?" << std::endl << "> ";
+	std::cout << "Qual o ID do novo funcionario? (int)" << std::endl << "> ";
 	std::cin >> id;
-	std::cout << "Qual o nome do novo funcionario?" << std::endl << "> ";
-	std::cin >> nome;
-	std::cout << "Qual o CPF do novo funcionario?" << std::endl << "> ";
-	std::cin >> cpf;
-	std::cout << "Qual a idade do novo funcionario?" << std::endl << "> ";
+	std::cout << "Qual o nome do novo funcionario? (string)" << std::endl << "> ";
+	std::cin.ignore();
+	std::getline(cin, nome);
+	std::cout << "Qual o CPF do novo funcionario? (string)" << std::endl << "> ";
+	std::cin.ignore();
+	std::getline(cin, cpf);
+	std::cout << "Qual a idade do novo funcionario? (short)" << std::endl << "> ";
 	std::cin >> idade;
-	std::cout << "Qual o tipo sanguinio do novo funcionario?" << std::endl << "> ";
-	std::cin >> tipoSanguineo;
-	std::cout << "Qual o fator Rh do novo funcionario?" << std::endl << "> ";
+	std::cout << "Qual o tipo sanguinio do novo funcionario? (string)" << std::endl << "> ";
+	std::cin.ignore();
+	std::getline(cin, tipoSanguineo);
+	std::cout << "Qual o fator Rh do novo funcionario? (char)" << std::endl << "> ";
 	std::cin >> fatorRh;
-	std::cout << "Qual eh a especialidade do novo funcionario?" << std::endl << "> ";
-	std::cin >> especialidade;
+	std::cout << "Qual eh a especialidade do novo funcionario? (string)" << std::endl << "> ";
+	std::cin.ignore();
+	std::getline(cin, especialidade);
 	if(opt == 'v') {
 		novoFuncionario = new Veterinario(id, nome, cpf, idade, tipoSanguineo, fatorRh, especialidade, CRMV);
 	}
@@ -257,47 +262,54 @@ void Interface::cadastrarAnimal() {
 	std::cout <<
 	std::endl <<
 	"Cadastro de Animal:" << std::endl;
-	std::cout << "Qual o ID do animal que estamos cadastrando?" << std::endl << "> ";
+	std::cout << "Qual o ID do animal que estamos cadastrando? (int)" << std::endl << "> ";
 	std::cin >> id;
-	std::cout << "Qual a classe do animal que estamos cadastrando?" << std::endl << "> ";
-	std::cin >> classe;
-	std::cout << "Qual o nome cientifico do animal que estamos cadastrando?" << std::endl << "> ";
-	std::cin >> nomeCientifico;
-	std::cout << "Qual o sexo do animal que estamos cadastrando?" << std::endl << "> ";
+	std::cout << "Qual a classe do animal que estamos cadastrando? (string)" << std::endl << "> ";
+	std::cin.ignore();
+	std::getline(cin, classe);
+	std::cout << "Qual o nome cientifico do animal que estamos cadastrando? (string)" << std::endl << "> ";
+	std::cin.ignore();
+	std::getline(cin, nomeCientifico);
+	std::cout << "Qual o sexo do animal que estamos cadastrando? (char)" << std::endl << "> ";
 	std::cin >> sexo;
-	std::cout << "Qual o tamanho do animal que estamos cadastrando?" << std::endl << "> ";
+	std::cout << "Qual o tamanho do animal que estamos cadastrando? (double)" << std::endl << "> ";
 	std::cin >> tamanho;
-	std::cout << "Qual a dieta do animal que estamos cadastrando?" << std::endl << "> ";
-	std::cin >> dieta;
-	std::cout << "Qual o id do veterinario do animal que estamos cadastrando?" << std::endl << "> ";
+	std::cout << "Qual a dieta do animal que estamos cadastrando? (string)" << std::endl << "> ";
+	std::cin.ignore();
+	std::getline(cin, dieta);
+	std::cout << "Qual o id do veterinario do animal que estamos cadastrando? (int)" << std::endl << "> ";
 	std::cin >> idVeterinario;
-	std::cout << "Qual o id do tratador do animal que estamos cadastrando?" << std::endl << "> ";
+	std::cout << "Qual o id do tratador do animal que estamos cadastrando? (int)" << std::endl << "> ";
 	std::cin >> idTratador;
-	std::cout << "Qual o nome de batismo do animal que estamos cadastrando?" << std::endl << "> ";
-	std::cin >> nomeBatismo;
+	std::cout << "Qual o nome de batismo do animal que estamos cadastrando? (string)" << std::endl << "> ";
+	std::cin.ignore();
+	std::getline(cin, nomeBatismo);
 	std::cout << "O animal eh um (a)nfibio, (m)amifero, (r)eptil ou a(v)e?" << std::endl << "> ";
 	std::cin >> anf_mam_rep_ave;
 	switch(anf_mam_rep_ave) {
 		case 'a':
-			std::cout << "Qual o total mudas do anfibio em questao?" << std::endl << "> ";
+			std::cout << "Qual o total mudas do anfibio em questao? (int)" << std::endl << "> ";
 			std::cin >> totalMudas;
-			std::cout << "Qual a ultima muda do anfibio em questao?" << std::endl << "> ";
+			std::cout << "Qual a ultima muda do anfibio em questao? (%**(*@#&@#+§))" << std::endl << "> ";
+			//std::cin.ignore();
 			//std::cin >> ultimaMuda;
 			break;
 		case 'm':
-			std::cout << "Qual o cor do pelo do mamifero em questao?" << std::endl << "> ";
-			std::cin >> corPelo;
+			std::cout << "Qual o cor do pelo do mamifero em questao? (string)" << std::endl << "> ";
+			std::cin.ignore();
+			std::getline(cin, corPelo);
 			break;
 		case 'r':
-			std::cout << "Esse reptil eh venenoso?" << std::endl << "> ";
+			std::cout << "Esse reptil eh venenoso? (bool 1 - 0)" << std::endl << "> ";
 			std::cin >> venenoso;
-			std::cout << "Qual o tipo de veneno desse reptil?" << std::endl << "> ";
-			std::cin >> tipoVeneno;
+			std::cout << "Qual o tipo de veneno desse reptil? (string)" << std::endl << "> ";
+			std::cin.ignore();
+			std::getline(cin, tipoVeneno);
 			break;
 		case 'v':
-			std::cout << "Qual o tamanho do bico dessa ave (em centimetros)?" << std::endl << "> ";
+			std::cout << "Qual o tamanho do bico dessa ave em centimetros? (double)" << std::endl << "> ";
 			std::cin >> tamanhoBicoCm;
-			std::cout << "Qual o envergadura das asas dessa ave?" << std::endl << "> ";
+			std::cout << "Qual o envergadura das asas dessa ave? (double)" << std::endl << "> ";
 			std::cin >> envergaduraAsas;
 			break;
 		default:
@@ -309,18 +321,21 @@ void Interface::cadastrarAnimal() {
 	std::cin >> silvestre_domestico;
 	switch(silvestre_domestico) {
 		case 's':
-			std::cout << "Qual a autorizacao do Ibama para o animal silvestre em questao?" << std::endl << "> ";
-			std::cin >> autorizacaoIbama;
+			std::cout << "Qual a autorizacao do Ibama para o animal silvestre em questao? (string)" << std::endl << "> ";
+			std::cin.ignore();
+			std::getline(cin, autorizacaoIbama);
 			std::cout << "Estamos tratando de um animal (n)ativo ou (e)xotico?" << std::endl << "> ";
 			std::cin >> nativo_exotico;
 			switch(nativo_exotico) {
 				case 'n':
-					std::cout << "Qual a UF do animal?" << std::endl << "> ";
-					std::cin >> ufOrigem;
+					std::cout << "Qual a UF do animal? (string)" << std::endl << "> ";
+					std::cin.ignore();
+					std::getline(cin, ufOrigem);
 					break;
 				case 'e':
-					std::cout << "Qual o pais de origem do animal?" << std::endl << "> ";
-					std::cin >> paisOrigem;
+					std::cout << "Qual o pais de origem do animal? (string)" << std::endl << "> ";
+					std::cin.ignore();
+					std::getline(cin, paisOrigem);
 					std::cout << "O animal eh um (a)nfibio, (m)amifero, (r)eptil ou a(v)e?" << std::endl << "> ";
 					std::cin >> anf_mam_rep_ave;
 					break;
